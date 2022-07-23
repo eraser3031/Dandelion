0f53f0d3e91870bc98b0cbe83eb853f15b129b67
eraser <eraser3031@gmail.com>
2022-07-23 23:01:16 +0900

    Feat: 에셋 추가

---
+++ Dandelion/Design/Asset.swift
@@ -0,0 +1,14 @@
+//
+//  Asset.swift
+//  Dandelion
+//
+//  Created by 김예훈 on 2022/07/23.
+//
+
+import SwiftUI
+
+extension Image {
+    static let logo = Image("Logo")
+    static let seed = Image("Seed")
+    static let book = Image("Book")
+}
