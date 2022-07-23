95018c634eef58982224dca6668570662203a223
eraser <eraser3031@gmail.com>
2022-07-23 19:47:14 +0900

    Initial Commit

---
+++ Dandelion/DandelionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DandelionApp.swift
+//  Dandelion
+//
+//  Created by 김예훈 on 2022/07/23.
+//
+
+import SwiftUI
+
+@main
+struct DandelionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
