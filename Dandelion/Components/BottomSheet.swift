2511cdd76d2b3e237d6e93cd431921d2aa461b24
eraser <eraser3031@gmail.com>
2022-07-26 20:05:43 +0900

    Feat: BottomSheet 추가

--- Dandelion/Components/BottomSheet.swift
+++ Dandelion/Components/BottomSheet.swift
@@ -7,14 +7,92 @@
 
 import SwiftUI
 
-struct BottomSheet: View {
+struct BottomSheet<Content: View>: View {
+    
+    @Binding var isPresented: Bool
+    let content: () -> Content
+    
+    init(isPresented: Binding<Bool>, @ViewBuilder content: @escaping () -> Content) {
+        self._isPresented = isPresented
+        self.content = content
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        content()
+            .padding(20)
+            .background(
+                RoundedRectangle(cornerRadius: 10, style: .continuous)
+                    .fill(Color.theme.background)
+            )
+    }
+}
+
+struct BottomSheetViewModifier<InnerContent: View>: ViewModifier {
+    
+    @Binding var isPresented: Bool
+    let innerContent: () -> InnerContent
+    
+    init(isPresented: Binding<Bool>, @ViewBuilder content: @escaping () -> InnerContent) {
+        self._isPresented = isPresented
+        self.innerContent = content
+    }
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+                .blur(radius: isPresented ? 20 : 0)
+            ZStack(alignment: .bottom) {
+                if isPresented {
+                    Color.black.opacity(0.2)
+                        .ignoresSafeArea()
+                        .onTapGesture {
+                            withAnimation(.spring()) {
+                                isPresented = false
+                            }
+                        }
+                        .transition(.opacity)
+                    
+                    BottomSheet(isPresented: $isPresented) {
+                        innerContent()
+                    }
+                    .transition(.move(edge: .bottom).combined(with: .opacity))
+                }
+            }
+            .ignoresSafeArea(.all, edges: .bottom)
+        }
+    }
+}
+
+extension View {
+    func bottomSheet<Content: View>(isPresented: Binding<Bool>, @ViewBuilder content: @escaping () -> Content) -> some View {
+        self.modifier(BottomSheetViewModifier(isPresented: isPresented, content: content))
+    }
+}
+
+struct BottomSheetPreView: View {
+    @State private var state = false
+    var body: some View {
+        ZStack {
+            Button {
+                withAnimation {
+                    state.toggle()
+                }
+            } label: {
+                Text("show")
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .bottomSheet(isPresented: $state) {
+            Text("hello~")
+                .frame(maxWidth: .infinity, maxHeight: 300)
+        }
+
     }
 }
 
 struct BottomSheet_Previews: PreviewProvider {
     static var previews: some View {
-        BottomSheet()
+        BottomSheetPreView()
     }
 }
